4ef70f24f58fea7c0d54e6e16feb833f492126ea
krugzkv <67159848+krugzkv@users.noreply.github.com>
2020-11-13 02:59:58 +0500

    date tryed

--- js/main.js
+++ js/main.js
@@ -178,7 +178,7 @@ const setPosts = {
         displayName: setUsers.user.displayName,
         photo: setUsers.user.photo,
       },
-      date: new Date().toLocaleDateString(),
+      date: formatDate(new Date),
       like: 0,
       comments: 0,
     })
@@ -188,6 +188,37 @@ const setPosts = {
 
   }
 }
+
+function formatDate(date) {
+  let dayOfMonth = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let diffMs = new Date() - date;
+  let diffSec = Math.round(diffMs / 1000);
+  let diffMin = diffSec / 60;
+  let diffHour = diffMin / 60;
+
+  // форматирование
+  year = year.toString().slice(-2);
+  month = month < 10 ? '0' + month : month;
+  dayOfMonth = dayOfMonth < 10 ? '0' + dayOfMonth : dayOfMonth;
+  hour = hour < 10 ? '0' + hour : hour;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+
+  if (diffSec < 1) {
+    return 'прямо сейчас';
+  } else if (diffMin < 1) {
+    return `${diffSec} сек. назад`
+  } else if (diffHour < 1) {
+    return `${diffMin} мин. назад`
+  } else {
+    return `${dayOfMonth}.${month}.${year} ${hour}:${minutes}`
+  }
+}
+
+console.log(formatDate);
 
 const showAllPosts = () => {
 
@@ -344,6 +375,7 @@ const init = () => {
 
     setPosts.addPost(title.value, text.value, tags.value, showAllPosts);
     toggleModal();
+    menu.classList.toggle('visible');
     addPostElem.reset();
   })
 }
