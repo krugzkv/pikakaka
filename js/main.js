8e7f55d5bad1960d6f210c2d9564f5ee04be8ff2
krugzkv <67159848+krugzkv@users.noreply.github.com>
2020-11-14 16:41:08 +0500

    lesson 4

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDyMO9iTXFmwFBhk-4XyvWjT8sAnkCchvA",
@@ -11,10 +10,6 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-console.log(firebase);
-
-
-
 
 let menuToggle = document.querySelector('#menu-toggle');
 let menu = document.querySelector('.sidebar');
@@ -41,6 +36,8 @@ const editContainer = document.querySelector('.edit-container');
 const editUsername = document.querySelector('.edit-username');
 const editPhotoUrl = document.querySelector('.edit-photo');
 const addPostElem = document.querySelector('.modal-addpost');
+const loginForget = document.querySelector('.login-forget');
+const DEFAULT_PHOTO = userAvatar.src;
 
 function validEmail(str) {
   const regEmail = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/
@@ -50,145 +47,6 @@ function validPassword(str) {
   const regPassword = /^(?=.*\d)(?=.*[a-zA-Z])(?!.*\s).*$/
   return regPassword.test(str);
 }
-
-
-const listUsers = [
-  {
-    id: '01',
-    email: 'eliza@mail.ru',
-    password: '12345qaZ',
-    displayName: 'eliza',
-    photo: 'https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
-  },
-  {
-    id: '02',
-    email: 'shadow@inbox.ru',
-    password: '123456qaZ',
-    displayName: 'Krugz',
-    photo: 'https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=911&q=80'
-  }
-];
-const setUsers = {
-  user: null,
-  logIn(email, password, handler) {
-    if (validEmail(email) && validPassword(password)) {
-      const user = this.getUser(email);
-      if (user && user.password === password) {
-        this.autorizedUser(user)
-        handler();
-      } else {
-        alert('Пользователь с такими данными не найден')
-
-        emailInput.classList.add('error');
-        passwordInput.classList.add('error');
-      }
-    } else {
-      alert('Проверте логин и пароль');
-      emailInput.classList.add('error');
-      passwordInput.classList.add('error');
-    }
-  },
-  logOut(handler) {
-    this.user = null;
-    handler();
-  },
-  signUp(email, password, handler) {
-    if (validEmail(email) && validPassword(password)) {
-      if (!this.getUser(email) && email.trim() && password.trim()) {
-        const user = { email, password, displayName: email.substring(0, email.indexOf('@')) };
-        listUsers.push(user);
-        this.autorizedUser(user);
-        handler();
-      }
-      else {
-        alert('Пользователь с таким email уже зарегестрирован')
-
-        emailInput.classList.add('error');
-        passwordInput.classList.add('error');
-      }
-    } else {
-      alert('Проверте логин и пароль');
-      emailInput.classList.add('error');
-      passwordInput.classList.add('error');
-    }
-  },
-  editUser(userName, userPhoto, handler) {
-    if (userName) {
-      this.user.displayName = userName;
-    }
-    if (userPhoto) {
-      this.user.photo = userPhoto;
-    }
-    handler();
-  },
-  getUser(email) {
-    return listUsers.find(item => item.email === email);
-  },
-  autorizedUser(user) {
-    this.user = user;
-  }
-};
-const toggleAuthDom = () => {
-  const user = setUsers.user;
-
-  if (user) {
-    loginElem.style.display = 'none';
-    userElem.style.display = '';
-    menuNav.style.display = '';
-    buttonNewPost.classList.add('visible');
-    userNameElem.textContent = user.displayName;
-    userAvatar.src = user.photo ? user.photo : userAvatar.src;
-  } else {
-    loginElem.style.display = '';
-    userElem.style.display = 'none';
-    menuNav.style.display = 'none';
-    buttonNewPost.classList.remove('visible');
-    addPostElem.classList.remove('visible');
-  }
-}
-
-const setPosts = {
-  allPosts: [
-    {
-      title: 'Как написать бред за 5 минут',
-      text: 'Бред преследования может выражаться в форме бреда отравления, притеснения, но наиболее частая его разновидность у пожилых — это так называемый бред ущерба, проявляющий обычно идеями воровства. Чаще всего бред ущерба возникает после 60–65 лет, хотя встречается и у более молодых (в 45–55 лет).Есть мнение, что некоторые категории людей, страдающих медленно прогрессирующими хроническими психическими заболеваниями, также склонны к развитию маломасштабного бреда. Вступая в зрелый или пожилой возраст, они легко могут формировать бред ущерба, воровства или иного преследования, который отличается относительной сложностью и необычностью: например, для них ущерб — это не просто пропажа вещей, а скорее их подмена или порча.Почему стареющая психика охотнее строит маломасштабные бредовые конструкции, до сих пор не очень ясно. Есть гипотеза, что в ней, как и во всем организме, происходят приспособительные изменения и она, отдаляясь от беспокойной внешней среды, компенсаторно сосредотачивается на том, что ближе, конкретнее, понятнее. Мы видим, что пожилой ограничивает свои контакты и становится менее активным.Все, что у него остается — это он сам, его окружение, его ближайшая среда.И бред, формирующийся в этих условиях, приобретает малый размах.',
-      tags: ['свежее', 'подписка', 'сумашедший', 'поравпсихушку'],
-      author: { displayName: 'eliza', photo: 'https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80' },
-      date: '11.11.2020, 20:54:00',
-      like: '15',
-      comments: '9',
-    },
-    {
-      title: 'Тут может быть ваша реклама',
-      text: 'Таким образом укрепление и развитие структуры требуют от нас анализа форм развития. Не следует, однако забывать, что реализация намеченных плановых заданий требуют от нас анализа позиций, занимаемых участниками в отношении поставленных задач. Задача организации, в особенности же консультация с широким активом способствует подготовки и реализации новых предложений. Равным образом новая модель организационной деятельности позволяет оценить значение модели развития.Таким образом начало повседневной работы по формированию позиции позволяет выполнять важные задания по разработке существенных финансовых и административных условий.Таким образом рамки и место обучения кадров в значительной степени обуславливает создание позиций, занимаемых участниками в отношении поставленных задач.Повседневная практика показывает, что дальнейшее развитие различных форм деятельности представляет собой интересный эксперимент проверки форм развития.Товарищи! новая модель организационной деятельности требуют определения и уточнения форм развития.Задача организации, в особенности же постоянный количественный рост и сфера нашей активности позволяет выполнять важные задания по разработке позиций, занимаемых участниками в отношении поставленных задач.Повседневная практика показывает, что дальнейшее развитие различных форм деятельности требуют определения и уточнения соответствующий условий активизации.',
-      tags: ['свежее', 'ёмоё', 'Hot', 'news', 'новое'],
-      author: { displayName: 'krugzkv', photo: 'https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=911&q=80' },
-      date: '01.11.2020, 10:54:00',
-      like: '55',
-      comments: '19',
-    },
-  ],
-  addPost(title, text, tags, handler) {
-
-    this.allPosts.unshift({
-      title,
-      text,
-      tags: tags.split(',').map(item => item.trim()),
-      author: {
-        displayName: setUsers.user.displayName,
-        photo: setUsers.user.photo,
-      },
-      date: formatDate(new Date),
-      like: 0,
-      comments: 0,
-    })
-    if (handler) {
-      handler();
-    }
-
-  }
-}
-
 function formatDate(date) {
   let dayOfMonth = date.getDate();
   let month = date.getMonth() + 1;
@@ -217,15 +75,168 @@ function formatDate(date) {
     return `${dayOfMonth}.${month}.${year} ${hour}:${minutes}`
   }
 }
+function toggleModal() {
+  modalNewPost.classList.toggle('visible');
+  if (modalNewPost.classList.contains('visible')) {
+    disabledScroll();
+  }
+  else {
+    modalNewPost.style.display = ('');
+    enabledScroll();
+  };
+  modalNewPost.addEventListener('click', function (event) {
+    if (event.target.classList.contains('visible')) {
+      toggleModal();
+    };
+  });
+};
 
-console.log(formatDate);
+const toggleAuthDom = () => {
+  const user = setUsers.user;
 
+  if (user) {
+    loginElem.style.display = 'none';
+    userElem.style.display = '';
+    menuNav.style.display = '';
+    buttonNewPost.classList.add('visible');
+    userNameElem.textContent = user.displayName;
+    userAvatar.src = user.photoURL || DEFAULT_PHOTO;
+  } else {
+    loginElem.style.display = '';
+    userElem.style.display = 'none';
+    menuNav.style.display = 'none';
+    buttonNewPost.classList.remove('visible');
+    addPostElem.classList.remove('visible');
+  }
+}
+const setUsers = {
+  user: null,
+  initUser(handler) {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.user = user;
+      } else {
+        this.user = null;
+      }
+      if (handler) {
+        handler();
+      };
+    })
+  },
+  logIn(email, password, handler) {
+    if (!email) {
+      alert('Проверте логин');
+    }
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then()
+      .catch(err => {
+        const errCode = err.code;
+        const errMessage = err.message;
+        if (errCode === 'auth/wrong-password') {
+          console.log(errMessage)
+          alert('Неверный пароль')
+        } else if (errCode === 'auth/user-not-found') {
+          console.log(errMessage)
+          alert('Пользователь не найден')
+        } else {
+          alert(errMessage)
+        }
+        console.log(err);
+      });
+    handler();
+  },
+  logOut() {
+    firebase.auth().signOut()
+      .then()
+      .catch();
+  },
+  signUp(email, password, handler) {
+    if (!validEmail(email) && !validPassword(password)) {
+      alert('Проверте логин и пароль');
+    }
+    firebase.auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(data => {
+        this.editUser(email.substring(0, email.indexOf('@')), null, handler);
+      })
+      .catch(err => {
+        const errCode = err.code;
+        const errMessage = err.message;
+        if (errCode === 'auth/weak-password') {
+          console.log(errMessage)
+          alert('Слабый пароль')
+        } else if (errCode === 'auth/email-already-in-use') {
+          console.log(errMessage)
+          alert('Этот email уже использоваться')
+        } else {
+          alert(errMessage)
+        }
+        console.log(err);
+      });
+  },
+  editUser(displayName, photoURL, handler) {
+    const user = firebase.auth().currentUser;
+    if (displayName) {
+      if (photoURL) {
+        user.updateProfile({
+          displayName,
+          photoURL
+        }).then(handler)
+      } else {
+        user.updateProfile({
+          displayName
+        }).then(handler)
+      }
+    }
+  },
+  sendForget(email) {
+    firebase.auth().sendPasswordResetEmail(email)
+      .then(() => {
+        alert('Письмо отправлено')
+      })
+      .catch(err => {
+        const errCode = err.code;
+        const errMessage = err.message;
+        if (errCode === 'auth/user-not-found') {
+          console.log(errMessage)
+          alert('Пользователь не найден')
+        } else {
+          alert(errMessage)
+        }
+        console.log(err);
+      })
+  }
+};
+const setPosts = {
+  allPosts: [],
+  addPost(title, text, tags, handler) {
+    const user = firebase.auth().currentUser;
+    this.allPosts.unshift({
+      id: `postID${(+new Date()).toString(16)}-${user.uid}`,
+      title,
+      text,
+      tags: tags.split(',').map(item => item.trim()),
+      author: {
+        displayName: setUsers.user.displayName,
+        photo: setUsers.user.photoURL,
+      },
+      date: formatDate(new Date),
+      like: 0,
+      comments: 0,
+    })
+    firebase.database().ref('post').set(this.allPosts)
+      .then(() => this.getPosts(handler))
+  },
+  getPosts(handler) {
+    firebase.database().ref('post').on('value', snapshot => {
+      this.allPosts = snapshot.val() || [];
+      handler();
+    })
+  }
+}
 const showAllPosts = () => {
-
   let postsHTML = '';
-
   setPosts.allPosts.forEach(({ title, text, date, like, comments, tags, author }) => {
-
     postsHTML += `
     <section class=post>
         <div class="post-body">
@@ -271,29 +282,14 @@ const showAllPosts = () => {
 </section >
   `;
   })
-
   postWrapper.innerHTML = postsHTML;
 };
-
-function toggleModal() {
-  modalNewPost.classList.toggle('visible');
-  if (modalNewPost.classList.contains('visible')) {
-    disabledScroll();
-  }
-  else {
-    modalNewPost.style.display = ('');
-    enabledScroll();
-  };
-  modalNewPost.addEventListener('click', function (event) {
-    if (event.target.classList.contains('visible')) {
-      toggleModal();
-    };
-  });
-};
-
-
 const init = () => {
-
+  loginForget.addEventListener('click', event => {
+    event.preventDefault();
+    setUsers.sendForget(emailInput.value);
+    emailInput.value = '';
+  })
   buttonNewPost.addEventListener('click', event => {
     event.preventDefault();
     toggleModal();
@@ -306,45 +302,30 @@ const init = () => {
     event.preventDefault();
     toggleModal();
     addPostElem.reset();
-
   })
-
-
   exit.addEventListener('click', event => {
     event.preventDefault();
     setUsers.logOut(toggleAuthDom);
-
   });
   loginForm.addEventListener('submit', event => {
     event.preventDefault();
-
     const emailValue = emailInput.value;
     const passwordValue = passwordInput.value;
-
     setUsers.logIn(emailValue, passwordValue, toggleAuthDom);
-    emailInput.classList.remove('error');
-    passwordInput.classList.remove('error');
     loginForm.reset();
   });
-
   loginSignup.addEventListener('click', event => {
     event.preventDefault();
-
     const emailValue = emailInput.value;
     const passwordValue = passwordInput.value;
-    emailInput.classList.remove('error');
-    passwordInput.classList.remove('error');
-
     setUsers.signUp(emailValue, passwordValue, toggleAuthDom);
     loginForm.reset();
   });
-
   editElem.addEventListener('click', event => {
     event.preventDefault();
     editContainer.classList.toggle('visible');
     editUsername.value = setUsers.user.displayName;
   });
-
   editContainer.addEventListener('submit', event => {
     event.preventDefault();
     setUsers.editUser(editUsername.value, editPhotoUrl.value, toggleAuthDom);
@@ -353,17 +334,12 @@ const init = () => {
   menuToggle.addEventListener('click', function (event) {
     event.preventDefault();
     menu.classList.toggle('visible');
-    emailInput.classList.remove('error');
-    passwordInput.classList.remove('error');
   });
-  showAllPosts();
-  toggleAuthDom();
-
-
+  setUsers.initUser(toggleAuthDom);
+  setPosts.getPosts(showAllPosts);
   addPostElem.addEventListener('submit', event => {
     event.preventDefault();
     const { title, text, tags } = addPostElem.elements;
-    console.log(title, text, tags);
     if (title.value.length < 6) {
       alert('Слишком короткий заголовок');
       return;
@@ -372,15 +348,12 @@ const init = () => {
       alert('Слишком короткий текст');
       return;
     }
-
     setPosts.addPost(title.value, text.value, tags.value, showAllPosts);
     toggleModal();
     menu.classList.toggle('visible');
     addPostElem.reset();
-  })
+  });
 }
-
-
 document.addEventListener('DOMContentLoaded', init)
 
 
